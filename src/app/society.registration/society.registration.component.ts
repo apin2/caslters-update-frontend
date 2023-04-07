a48bf7a4d541bdaf14a2f787ae88bece99127adc
Nitin Rawat <nitinr426@gmail.com>
2023-04-07 15:29:01 +0530

    07April

--- src/app/society.registration/society.registration.component.ts
+++ src/app/society.registration/society.registration.component.ts
@@ -83,8 +83,12 @@ export class SocietyRegistrationComponent {
     return this.userForm.controls;
   }
 
-  save(f:any){
-    if(f.value.societyName != '' && f.value.societyRegistrationNumber != '' && f.value.societyRegistrationNumber != ''){
+  save(f: any) {
+    if (
+      f.value.societyName != '' &&
+      f.value.societyRegistrationNumber != '' &&
+      f.value.societyRegistrationNumber != ''
+    ) {
       console.log(f.value);
       // console.log(this.socitybearer);
       let obj = {
@@ -99,10 +103,10 @@ export class SocietyRegistrationComponent {
         existingMemberCount: f.value.existingMemberCount,
         age: f.value.ageofbuilding,
         societyRegisteredCode:
-          'MHP' +
-          f.value.societyRegistrationNumber +
+          'MH' +
+          f.value.city[0] +
           Math.floor(Math.random() * (90 - 10 + 1)) +
-          10,
+          100,
         societyMemberDetails: this.socitybearer,
       };
       console.log('obj====>>', obj);
@@ -115,9 +119,6 @@ export class SocietyRegistrationComponent {
         console.log(res);
       });
       this.route.navigate(['selectSociety']);
-    }
-    else{
-      this.toastr.error( 'is Requered',"Fields" );
     } else {
       this.toastr.error('Is Required', 'societyName');
     }
@@ -125,100 +126,100 @@ export class SocietyRegistrationComponent {
   bearersocity: any = [];
 
   // Office Bearers of Society member adding condition wise
-  bearerssave(bearers:any,name:any,email:any,number:any){
-    if(bearers != '' && name != '' && number != ''){
   bearerssave(bearers: any, name: any, email: any, number: any) {
-    this.userForm.controls['name'].reset();
-    this.userForm.controls['email'].reset();
-    this.userForm.controls['number'].reset();
-    this.bearers = new Baerer();
-    this.bearers.societyMemberDesignationId = bearers;
-    this.bearers.memberName = name;
-    this.bearers.email = email;
-    this.bearers.mobileNumber = number;
-    this.bearers.societyMemberDetailsId = 0;
-    this.bearers.registeredSocietyId = 0;
-    // if(this.socitybearer.length>0)
-    if (this.id || this.id == 0) {
-      for (let i = 0; i <= this.socitybearer.length; i++) {
-        if (this.id == i) {
-          this.socitybearer[i].societyMemberDesignationId =
-            this.bearers.societyMemberDesignationId;
-          this.socitybearer[i].memberName = this.bearers.memberName;
-          this.socitybearer[i].email = this.bearers.email;
-          this.socitybearer[i].mobileNumber = this.bearers.mobileNumber;
-          this.id = undefined;
+    if (bearers != '' && name != '' && number != '') {
+      this.userForm.controls['name'].reset();
+      this.userForm.controls['email'].reset();
+      this.userForm.controls['number'].reset();
+      this.bearers = new Baerer();
+      this.bearers.societyMemberDesignationId = bearers;
+      this.bearers.memberName = name;
+      this.bearers.email = email;
+      this.bearers.mobileNumber = number;
+      this.bearers.societyMemberDetailsId = 0;
+      this.bearers.registeredSocietyId = 0;
+      // if(this.socitybearer.length>0)
+      if (this.id || this.id == 0) {
+        for (let i = 0; i <= this.socitybearer.length; i++) {
+          if (this.id == i) {
+            this.socitybearer[i].societyMemberDesignationId =
+              this.bearers.societyMemberDesignationId;
+            this.socitybearer[i].memberName = this.bearers.memberName;
+            this.socitybearer[i].email = this.bearers.email;
+            this.socitybearer[i].mobileNumber = this.bearers.mobileNumber;
+            this.id = undefined;
+          }
         }
-      }
-    } else {
-      if (!['1', '2', '3'].includes(this.bearers.societyMemberDesignationId)) {
-        this.socitybearer.push(this.bearers);
-        // for(let i=0;i<=this.socitybearer.length;i++){
-        //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
-        //    }
-        // }
       } else {
         if (
-          !this.check.chairman &&
-          this.bearers.societyMemberDesignationId === '1'
+          !['1', '2', '3'].includes(this.bearers.societyMemberDesignationId)
         ) {
-          this.check.chairman = true;
           this.socitybearer.push(this.bearers);
           // for(let i=0;i<=this.socitybearer.length;i++){
           //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
-          //   }
-          //   else{
-
-          //   }
-          // }
-          console.log('this.', this.socitybearer);
-        } else if (
-          !this.check.Secretory &&
-          this.bearers.societyMemberDesignationId === '2'
-        ) {
-          this.check.Secretory = true;
-          this.socitybearer.push(this.bearers);
-          // for(let i=0;i<=this.socitybearer.length;i++){
-          //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
-          //   }
-          //   else{
-
-          //   }
-          // }
-        } else if (
-          !this.check.treasurer &&
-          this.bearers.societyMemberDesignationId === '3'
-        ) {
-          this.check.treasurer = true;
-          this.socitybearer.push(this.bearers);
-          // for(let i=0;i<=this.socitybearer.length;i++){
-          //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
-          //   }
-          //   else{
-
-          //   }
+          //    }
           // }
         } else {
-          console.log(
-            `${this.bearers.societyMemberDesignationId} is already Exist`
-          );
-          //  console.log("this.toastr.success('Hello world!', 'Toastr fun!')",this.toastr.success('Hello world!', 'Toastr fun!'))
-          if (this.bearers.societyMemberDesignationId == '1') {
-            let alertmessage = 'chairman';
-            this.toastr.error('is already Exist!', alertmessage);
-          } else if (this.bearers.societyMemberDesignationId == '2') {
-            let alertmessage = 'Secretory';
-            this.toastr.error('is already Exist!', alertmessage);
-          } else if (this.bearers.societyMemberDesignationId == '3') {
-            let alertmessage = 'Treasurer';
-            this.toastr.error('is already Exist!', alertmessage);
+          if (
+            !this.check.chairman &&
+            this.bearers.societyMemberDesignationId === '1'
+          ) {
+            this.check.chairman = true;
+            this.socitybearer.push(this.bearers);
+            // for(let i=0;i<=this.socitybearer.length;i++){
+            //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
+            //   }
+            //   else{
+
+            //   }
+            // }
+            console.log('this.', this.socitybearer);
+          } else if (
+            !this.check.Secretory &&
+            this.bearers.societyMemberDesignationId === '2'
+          ) {
+            this.check.Secretory = true;
+            this.socitybearer.push(this.bearers);
+            // for(let i=0;i<=this.socitybearer.length;i++){
+            //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
+            //   }
+            //   else{
+
+            //   }
+            // }
+          } else if (
+            !this.check.treasurer &&
+            this.bearers.societyMemberDesignationId === '3'
+          ) {
+            this.check.treasurer = true;
+            this.socitybearer.push(this.bearers);
+            // for(let i=0;i<=this.socitybearer.length;i++){
+            //   if(this.socitybearer.email != this.bearers.email && this.socitybearer.mobileNumber != this.bearers.mobileNumber){
+            //   }
+            //   else{
+
+            //   }
+            // }
+          } else {
+            console.log(
+              `${this.bearers.societyMemberDesignationId} is already Exist`
+            );
+            //  console.log("this.toastr.success('Hello world!', 'Toastr fun!')",this.toastr.success('Hello world!', 'Toastr fun!'))
+            if (this.bearers.societyMemberDesignationId == '1') {
+              let alertmessage = 'chairman';
+              this.toastr.error('is already Exist!', alertmessage);
+            } else if (this.bearers.societyMemberDesignationId == '2') {
+              let alertmessage = 'Secretory';
+              this.toastr.error('is already Exist!', alertmessage);
+            } else if (this.bearers.societyMemberDesignationId == '3') {
+              let alertmessage = 'Treasurer';
+              this.toastr.error('is already Exist!', alertmessage);
+            }
           }
         }
       }
-    }
-  }
-    }else{
-      this.toastr.error( 'is Requered',"Fields" );
+    } else {
+      this.toastr.error('is Requered', 'Fields');
     }
   }
 
