c091174b558b630c9d7177f04aaca8352a3e8a14
Nitin Rawat <nitinr426@gmail.com>
2023-04-09 18:21:48 +0530

    sunday

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,70 +1,76 @@
 import { Component } from '@angular/core';
-import {  Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LoginService } from '../shared/interface/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  first_stage:any=true;
-  second_stage:any=false;
-  user_name:any;
-  mobile_number:any;
-  otp:any;
-  resend:any=false;
+  first_stage: any = true;
+  second_stage: any = false;
+  username: any;
+  mobile_number: any;
+  otp: any;
+  resend: any = false;
   display: any;
-  invalidsend:any=false;
+  invalidsend: any = false;
   public timerInterval: any;
-  password:any;
-  constructor(private toster:ToastrService,private loginservice:LoginService, private routers:Router){}
-  ngOnInit(){
-
-  }
-  login(l:any){
+  password: any;
+  constructor(
+    private toster: ToastrService,
+    private loginservice: LoginService,
+    private routers: Router
+  ) {}
+  ngOnInit() {}
+  login(l: any) {
     console.log(l.value);
-    let obj:any ={
-      username:l.value.user_name,
-      password:l.val.password
-    }
-     this.loginservice.login(obj).subscribe((res)=>{
-      if(res){
-        localStorage.setItem('user',res);
+    let obj: any = {
+      username: l.value.user_name,
+      password: l.value.password,
+    };
+    console.log(obj);
+
+    this.loginservice.login(obj).subscribe((res) => {
+      console;
+      if (res.role) {
+        console.log(res);
+        let user = JSON.stringify(res);
+        localStorage.setItem('user', user);
         this.routers.navigateByUrl('/');
+      } else {
+        this.toster.error('invalid user name and password', 'Invalid');
       }
-      this.toster.error('invalid user name and password','Invalid')
-    })
+    });
   }
-  next(){
-    if(this.user_name == 'pradeep'){
-    this.first_stage=false;
-    this.second_stage=true;
-    console.log(this.first_stage,this.second_stage);
-    }
-    else{
-      this.toster.error('user name does not  exist','Invalid user name');
-    }
-  }
-  send(){
-    if(this.mobile_number.length >= 10){
-      this.toster.success('success');
-      this.second_stage=false;
-      console.log(this.mobile_number);
-    this.timer(1);
-    }else{
-      this.toster.error("error");
-    }
-    
-  } 
+  // next() {
+  //   if (this.username == 'pradeep') {
+  //     this.first_stage = false;
+  //     this.second_stage = true;
+  //     console.log(this.first_stage, this.second_stage);
+  //   } else {
+  //     this.toster.error('user name does not  exist', 'Invalid user name');
+  //   }
+  // }
+  // send() {
+  //   if (this.mobile_number.length >= 10) {
+  //     this.toster.success('success');
+  //     this.second_stage = false;
+  //     console.log(this.mobile_number);
+  //     this.timer(1);
+  //   } else {
+  //     this.toster.error('error');
+  //   }
+  // }
 
-  reSend(){
-    this.resend=false;
-    this.timer(1);
-  }
+  // reSend() {
+  //   this.resend = false;
+  //   this.timer(1);
+  // }
 
-  timer(minute:any) {
+  timer(minute: any) {
     // let minute = 1;
     let seconds: number = minute * 60;
     let textSec: any = '0';
@@ -85,7 +91,7 @@ export class LoginComponent {
 
       if (seconds == 0) {
         console.log('finished');
-        this.resend=true;
+        this.resend = true;
         clearInterval(this.timerInterval);
       }
     }, 1000);
--- src/app/shared/interface/auth/auth.guard.ts
+++ src/app/shared/interface/auth/auth.guard.ts
@@ -1,47 +1,57 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   userinfo: any;
-  userdetail:any;
-  constructor(private auth: AuthService, private router: Router) { }
+  userdetail: any;
+  constructor(private auth: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
     // this.roleAccess(route.data['roles']);
     // console.log(this.roleAccess(route.data['roles']));
 
     if (this.auth.isLoggedIn()) {
-      if(this.roleAccess(route.data['roles'])){
-        return true
-      }else{
-        return false
+      if (this.roleAccess(route.data['roles'])) {
+        return true;
+      } else {
+        return false;
       }
       // return true
-    }else{
-    this.router.navigate(['/login']);
-    return false
-  }
-  }
-
-  roleAccess(role: any): any {
-    console.log("roleacess",role[0],role[1]);
-    // user role active logic 
-    
-    this.userdetail= localStorage.getItem('user')
-    if(role[0] == this.userdetail.role ||role[1] == this.userdetail.role ){
-      console.log("if");
-      return true
-    }else{
-      console.log("else");
-      
-      return false
+    } else {
+      this.router.navigate(['/login']);
+      return false;
     }
   }
 
+  roleAccess(role: any): any {
+    console.log('roleacess', role[0], role[1]);
+    // user role active logic
+    let user: any = localStorage.getItem('user');
+    this.userdetail = JSON.parse(user);
+    if (role[0] == this.userdetail.role || role[1] == this.userdetail.role) {
+      console.log('if');
+      return true;
+    } else {
+      console.log('else');
+
+      return false;
+    }
+  }
 }
